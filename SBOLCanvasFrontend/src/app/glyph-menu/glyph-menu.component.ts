787c0d5cffd5ffda97004dd19d4e54f642a3aab5
Sam Thayer <samonella7@gmail.com>
2019-04-18 16:34:07 -0600

    Less than ideal formatting for glyph dragsources

--- SBOLCanvasFrontend/src/app/glyph-menu/glyph-menu.component.ts
+++ SBOLCanvasFrontend/src/app/glyph-menu/glyph-menu.component.ts
@@ -7,29 +7,68 @@ import {GraphService} from '../graph.service';
   styleUrls: ['./glyph-menu.component.css']
 })
 export class GlyphMenuComponent implements OnInit {
+  glyphs: String[] = [];
 
   @ViewChild('glyphMenuContainer') glyphMenuContainer: ElementRef;
+  @ViewChild('glyphTileContainer') glyphTiles: ElementRef[];
 
   constructor(private graphService: GraphService) {
   }
 
   ngOnInit() {
     const glyphMenuContainer = this.glyphMenuContainer.nativeElement;
-    const dragSource = this.graphService.createGlyphDragSource();
-    glyphMenuContainer.appendChild(dragSource);
 
+    // Since this code happens on front end, it has no way to know
+    // what files are available - we'll need a service to do this properly.
+    // For now, use hardcoded list of image names
+    const glyphPics = [
+      'assets/glyphs/aptamer-specification.png',
+      'assets/glyphs/assembly-scar-specification-doublestrand.png',
+      'assets/glyphs/blunt-restriction-site-specification.png',
+      'assets/glyphs/cds-arrow-specification.png',
+      'assets/glyphs/cds-specification.png',
+      'assets/glyphs/composite-specification.png',
+      'assets/glyphs/dna-stability-element-specification.png',
+      'assets/glyphs/engineered-region-specification.png',
+      'assets/glyphs/five-prime-overhang-specification.png',
+      'assets/glyphs/five-prime-sticky-restriction-site-specification.png',
+      'assets/glyphs/halfround-rectangle-specification.png',
+      'assets/glyphs/insulator-specification.png',
+      'assets/glyphs/location-dna-no-top-specification.png',
+      'assets/glyphs/location-dna-specification.png',
+      'assets/glyphs/location-protein-no-top-specification.png',
+      'assets/glyphs/location-protein-specification.png',
+      'assets/glyphs/location-rna-no-top-specification.png',
+      'assets/glyphs/location-rna-specification.png',
+      'assets/glyphs/ncrna-specification.png',
+      'assets/glyphs/no-glyph-assigned-specification.png',
+      'assets/glyphs/nuclease-site-specification.png',
+      'assets/glyphs/omitted-detail-specification.png',
+      'assets/glyphs/operator-specification.png',
+      'assets/glyphs/origin-of-replication-specification.png',
+      'assets/glyphs/origin-of-transfer-specification.png',
+      'assets/glyphs/polyA-specification.png',
+      'assets/glyphs/primer-binding-site-specification.png',
+      'assets/glyphs/promoter-specification.png',
+      'assets/glyphs/protease-site-specification.png',
+      'assets/glyphs/protein-stability-element-specification.png',
+      'assets/glyphs/replacement-glyph-specification.png',
+      'assets/glyphs/ribonuclease-site-specification.png',
+      'assets/glyphs/ribosome-entry-site-specification.png',
+      'assets/glyphs/rna-stability-element-specification.png',
+      'assets/glyphs/signature-specification.png',
+      'assets/glyphs/specific-recombination-site-specification.png',
+      'assets/glyphs/terminator-specification.png'
+    ];
 
-    // const graph = this.graphService.getGraph();
-    // const parent = graph.getDefaultParent();
+    for (const imgName of glyphPics) {
+      const dragSource = this.graphService.createGlyphDragSource(imgName);
+      this.glyphMenuContainer.nativeElement.appendChild(dragSource);
+      //this.glyphs.push(imgName);
+    }
 
-    // graph.getModel().beginUpdate();
-    // try {
-    //   const v1 = graph.insertVertex(parent, null, 'GlyphMenu,', 20, 60, 80, 30);
-    //   const v2 = graph.insertVertex(parent, null, 'Works!', 200, 190, 80, 30);
-    //   graph.insertEdge(parent, null, '', v1, v2);
-    //   graph.refresh(v1);
-    // } finally {
-    //   graph.getModel().endUpdate();
+    // for (const tile of this.glyphTiles) {
+    //   tile.nativeElement.innerHTML = "Hi";
     // }
   }
 
--- SBOLCanvasFrontend/src/app/graph.service.ts
+++ SBOLCanvasFrontend/src/app/graph.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-//import {mxGraph, mxDragSource, mxCodec} from '../mxgraph';
 import * as mxGraph from 'mxgraph';
 import * as mxDragSource from 'mxgraph';
 
@@ -9,6 +8,12 @@ const mx = require('mxgraph')({
   mxBasePath: 'mxgraph'
 });
 
+// Constants (there is no doubt a better way to do this
+const glyphWidth = 60;
+const glyphHeight = 60;
+const glyphWidthStr = glyphWidth + 'px';
+const glyphHeightStr = glyphHeight + 'px';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,11 +22,13 @@ export class GraphService {
   graph: mxGraph;
   graphContainer: HTMLElement;
   glyphDragPreviewElt: HTMLElement;
+  baseGlyphStyle;
 
   constructor() {
 
     this.graphContainer = document.createElement('div');
     this.graphContainer.id = 'graphContainer';
+    this.graphContainer.style.margin = 'auto';
 
     this.graph = new mx.mxGraph(this.graphContainer);
 
@@ -31,19 +38,22 @@ export class GraphService {
 
     // A dummy element used for previewing glyphs as they are dragged onto the graph
     this.glyphDragPreviewElt = document.createElement('div');
-    this.glyphDragPreviewElt.style.border = 'dashed black 1px';
-    this.glyphDragPreviewElt.style.width = '120px';
-    this.glyphDragPreviewElt.style.height = '40px';
+    this.glyphDragPreviewElt.style.border = 'dashed white 1px';
+    this.glyphDragPreviewElt.style.width = glyphWidthStr;
+    this.glyphDragPreviewElt.style.height = glyphHeightStr;
 
-    const parent = this.graph.getDefaultParent();
-    this.graph.getModel().beginUpdate();
-    try {
-      const v1 = this.graph.insertVertex(parent, null, 'Canvas,', 20, 20, 80, 30);
-      const v2 = this.graph.insertVertex(parent, null, 'Works!', 200, 150, 80, 30);
-      this.graph.insertEdge(parent, null, '', v1, v2);
-    } finally {
-      this.graph.getModel().endUpdate();
-    }
+    this.baseGlyphStyle = new Object();
+    this.baseGlyphStyle[mx.mxConstants.STYLE_SHAPE] = mx.mxConstants.SHAPE_IMAGE;
+    this.baseGlyphStyle[mx.mxConstants.STYLE_PERIMETER] = mx.mxPerimeter.RectanglePerimeter;
+    this.baseGlyphStyle[mx.mxConstants.STYLE_FONTCOLOR] = '#FFFFFF';
+    this.baseGlyphStyle[mx.mxConstants.STYLE_SHAPE] = mx.mxConstants.SHAPE_LABEL;
+    this.baseGlyphStyle[mx.mxConstants.STYLE_STROKECOLOR] = '#000000';
+    this.baseGlyphStyle[mx.mxConstants.STYLE_ALIGN] = mx.mxConstants.ALIGN_CENTER;
+    this.baseGlyphStyle[mx.mxConstants.STYLE_VERTICAL_ALIGN] = mx.mxConstants.ALIGN_BOTTOM;
+    this.baseGlyphStyle[mx.mxConstants.STYLE_IMAGE_ALIGN] = mx.mxConstants.ALIGN_CENTER;
+    this.baseGlyphStyle[mx.mxConstants.STYLE_IMAGE_VERTICAL_ALIGN] = mx.mxConstants.ALIGN_TOP;
+    this.baseGlyphStyle[mx.mxConstants.STYLE_IMAGE_WIDTH] = String(glyphWidth);
+    this.baseGlyphStyle[mx.mxConstants.STYLE_IMAGE_HEIGHT] = String(glyphHeight);
   }
 
   /**
@@ -53,26 +63,33 @@ export class GraphService {
     return this.graphContainer;
   }
 
-  insertGlyph(graph, evt, target, x, y) {
-    const cell = new mx.mxCell('Test', new mx.mxGeometry(0, 0, 120, 40));
-    cell.vertex = true;
-    const cells = graph.importCells([cell], x, y, target);
-    console.log(target);
+  /**
+   * Creates a dragsource that can be used to add glyphs to the canvas,
+   * and returns the html element associated with it
+   */
+  createGlyphDragSource(sourceImg) {
+    const elt = document.createElement('img');
+    elt.src = sourceImg;
+    elt.width = glyphWidth;
+    elt.height = glyphHeight;
 
-    if (cells != null && cells.length > 0) {
-      graph.scrollCellToVisible(cells[0], false);
-      graph.setSelectionCells(cells);
+    const newGlyphStyle = mx.mxUtils.clone(this.baseGlyphStyle);
+    newGlyphStyle[mx.mxConstants.STYLE_IMAGE] = sourceImg;
+    const styleName = 'cellStyle:' + sourceImg;
+    this.graph.getStylesheet().putCellStyle(styleName, newGlyphStyle);
+
+    const insertGlyph = function (graph, evt, target, x, y) {
+      // When executed, 'this' is the dragSource, not the graphService
+
+      graph.getModel().beginUpdate();
+      try {
+        graph.insertVertex(graph.getDefaultParent(), null, '', x, y, glyphWidth, glyphHeight, styleName);
+      } finally {
+        graph.getModel().endUpdate();
+      }
     }
-  }
 
-  createGlyphDragSource() {
-    const elt = document.createElement('p');
-    elt.innerHTML = 'Drag me';
-    elt.style.width = '48px';
-    elt.style.height = '48px';
-    elt.style.backgroundColor = 'red';
-
-    const ds: mxDragSource = mx.mxUtils.makeDraggable(elt, this.graph, this.insertGlyph, this.glyphDragPreviewElt);
+    const ds: mxDragSource = mx.mxUtils.makeDraggable(elt, this.graph, insertGlyph, this.glyphDragPreviewElt);
     ds.isGridEnabled = function() {
       return this.graph.graphHandler.guidesEnabled;
     };
@@ -90,7 +107,7 @@ export class GraphService {
 
   graphToString(): string {
     var encoder = new mx.mxCodec();
-    var result = encoder.encode(this.graph.getModel()); 
+    var result = encoder.encode(this.graph.getModel());
     var xml = mx.mxUtils.getXml(result);
     return xml;
   }
@@ -105,68 +122,71 @@ export class GraphService {
     this.graph.getModel().beginUpdate();
     let vertecies = new Map<number, any>();
     let edges = [];
-    while (elt != null){
-      if(elt.attributes.getNamedItem("value") == null){
+    while (elt != null) {
+      if (elt.attributes.getNamedItem('value') == null) {
         elt = elt.nextSibling;
         continue;
       }
-      var id = elt.attributes.getNamedItem("id").value;
-      var value = elt.attributes.getNamedItem("value").value;
-      if(elt.attributes.getNamedItem("vertex") != null){
+      var id = elt.attributes.getNamedItem('id').value;
+      var value = elt.attributes.getNamedItem('value').value;
+      if (elt.attributes.getNamedItem('vertex') != null) {
         var geo = elt.firstChild;
-        var x = geo.attributes.getNamedItem("x").value;
-        var y = geo.attributes.getNamedItem("y").value;
-        var width = geo.attributes.getNamedItem("width").value;
-        var height = geo.attributes.getNamedItem("height").value;
+        var x = geo.attributes.getNamedItem('x').value;
+        var y = geo.attributes.getNamedItem('y').value;
+        var width = geo.attributes.getNamedItem('width').value;
+        var height = geo.attributes.getNamedItem('height').value;
         vertecies.set(id, this.graph.insertVertex(this.graph.getDefaultParent(), null, value, x, y, width, height));
-      }else if(elt.attributes.getNamedItem("edge") != null){
+      } else if (elt.attributes.getNamedItem('edge') != null) {
         var geo = null;
         var source = null;
-        if(elt.attributes.getNamedItem("source") != null){
-          source = elt.attributes.getNamedItem("source").value;
-        }else{
+        if (elt.attributes.getNamedItem('source') != null) {
+          source = elt.attributes.getNamedItem('source').value;
+        } else {
           geo = elt.firstChild.firstChild;
-          var x = geo.attributes.getNamedItem("x").value;
-          var y = geo.attributes.getNamedItem("y").value;
-          source = [x,y];
+          var x = geo.attributes.getNamedItem('x').value;
+          var y = geo.attributes.getNamedItem('y').value;
+          source = [x, y];
         }
         var target = null;
-        if(elt.attributes.getNamedItem("target") != null){
-          target = elt.attributes.getNamedItem("target").value;
-        }else{
-          if(geo == null){
+        if (elt.attributes.getNamedItem('target') != null) {
+          target = elt.attributes.getNamedItem('target').value;
+        } else {
+          if (geo == null) {
             geo = elt.firstChild.firstChild;
-          }else{
+          } else {
             geo = geo.nextSibling;
           }
-          var x = geo.attributes.getNamedItem("x").value;
-          var y = geo.attributes.getNamedItem("y").value;
-          target = [x,y];
+          var x = geo.attributes.getNamedItem('x').value;
+          var y = geo.attributes.getNamedItem('y').value;
+          target = [x, y];
         }
         edges.push([value, source, target]);
       }
       elt = elt.nextSibling;
     }
     // edges have to have their source and target set after all the vertecies have been read in
-    for(var i = 0; i < edges.length; i++){
+    for (var i = 0; i < edges.length; i++) {
       var source = null;
       var target = null;
-      if(!(edges[i][1] instanceof Array))
+      if (!(edges[i][1] instanceof Array)) {
         source = vertecies.get(edges[i][1]);
-      if(!(edges[i][2] instanceof Array))
+      }
+      if (!(edges[i][2] instanceof Array)) {
         target = vertecies.get(edges[i][2]);
+      }
       const edge = this.graph.insertEdge(this.graph.getDefaultParent(), null, edges[i][0], source, target);
       // if you remove the /1's below the positioning of the edge scales up by 10
       // we don't want any scaling, so there it shall stay
       // this sure is fun
-      if(edges[i][1] instanceof Array)
-        edge.geometry.setTerminalPoint(new mx.mxPoint(edges[i][1][0]/1, edges[i][1][1]/1), true);
-      if(edges[i][2] instanceof Array)
-        edge.geometry.setTerminalPoint(new mx.mxPoint(edges[i][2][0]/1, edges[i][2][1]/1), false);
+      if (edges[i][1] instanceof Array) {
+        edge.geometry.setTerminalPoint(new mx.mxPoint(edges[i][1][0] / 1, edges[i][1][1] / 1), true);
+      }
+      if (edges[i][2] instanceof Array) {
+        edge.geometry.setTerminalPoint(new mx.mxPoint(edges[i][2][0] / 1, edges[i][2][1] / 1), false);
+      }
       edge.relative = true;
     }
     this.graph.getModel().endUpdate();
     this.graph.refresh();
   }
-
 }
