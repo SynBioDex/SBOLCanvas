44456435cc3cad13796fdb65694638eb5b6d604d
Sam Thayer <samonella7@gmail.com>
2019-04-20 15:32:08 -0600

    Groups glyph and its ports for the color/info menu perspective

--- SBOLCanvasFrontend/src/app/glyphInfo.ts
+++ SBOLCanvasFrontend/src/app/glyphInfo.ts
@@ -1,4 +1,17 @@
 export class GlyphInfo {
   name: string;
   description: string;
+
+  makeCopy() {
+    const copy: GlyphInfo = new GlyphInfo();
+    copy.name = this.name;
+    copy.description = this.description;
+    return copy;
+  }
+
+  copyDataFrom(other: GlyphInfo) {
+    this.name = other.name;
+    this.description = other.description;
+  }
+
 }
