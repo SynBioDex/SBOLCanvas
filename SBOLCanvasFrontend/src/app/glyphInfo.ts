1bc8f3a5565486f716c84f7cc84422497f93fa47
Randoom97 <randoom97@live.com>
2019-09-06 17:47:57 -0600

    Frontend fields

--- SBOLCanvasFrontend/src/app/glyphInfo.ts
+++ SBOLCanvasFrontend/src/app/glyphInfo.ts
@@ -1,17 +1,29 @@
 export class GlyphInfo {
+  partType: string;
+  partRole: string;
+  displayID: string;
   name: string;
   description: string;
+  version: string;
 
   makeCopy() {
     const copy: GlyphInfo = new GlyphInfo();
+    copy.partType = this.partType;
+    copy.partRole = this.partRole;
+    copy.displayID = this.displayID;
     copy.name = this.name;
     copy.description = this.description;
+    copy.version = this.version;
     return copy;
   }
 
   copyDataFrom(other: GlyphInfo) {
+    this.partType = other.partType;
+    this.partRole = other.partRole;
+    this.displayID = other.displayID;
     this.name = other.name;
     this.description = other.description;
+    this.version = other.version;
   }
 
 }
--- SBOLCanvasFrontend/src/app/info-editor/info-editor.component.ts
+++ SBOLCanvasFrontend/src/app/info-editor/info-editor.component.ts
@@ -3,6 +3,7 @@ import { GlyphInfo } from '../glyphInfo';
 import {FormGroup} from '@angular/forms';
 import {MetadataService} from '../metadata.service';
 import {GraphService} from '../graph.service';
+import { MatSelectChange } from '@angular/material';
 
 
 @Component({
@@ -13,6 +14,34 @@ import {GraphService} from '../graph.service';
 
 export class InfoEditorComponent implements OnInit {
 
+  //TODO get these from the backend
+partTypes:[string, string][] = [
+  // name and uri pairs
+  ["All_Types","1"],
+  ["DNA","2"],
+  ["Complex","3"],
+  ["Effector","4"],
+  ["Protien","5"],
+  ["RNA","6"],
+  ["Small_molecule","7"]
+];
+
+//TODO get these from the backend (will depend on type) will also determine rendered glyph
+partRoles:[string, string][] =[
+  ["Pro (promoter)","1"],
+  ["Ter (terminator)", "2"]
+];
+
+//TODO get these from the backend (will depend on role)
+partRefinements:[string, string][] = [
+
+];
+
+//TODO get these from the backend
+encodings:[string, string][] = [
+
+];
+
   glyphInfo: GlyphInfo;
 
   constructor(private graphService: GraphService, private metadataService: MetadataService) { }
@@ -21,10 +50,40 @@ export class InfoEditorComponent implements OnInit {
     this.metadataService.selectedGlyphInfo.subscribe(glyphInfo => this.glyphInfoUpdated(glyphInfo));
   }
 
-  onChange(event: any) {
+  dropDownChange(event: MatSelectChange){
+    const id = event.source.id;
+
+    switch(id){
+      case 'partType':{
+        this.glyphInfo.partType = event.value;
+        break;
+      }
+      case 'partRole':{
+        this.glyphInfo.partRole = event.value;
+        break;
+      }
+      case 'partRefinement':{
+        if(event.value != "none"){
+          this.glyphInfo.partRole = event.value;
+        }
+        break;
+      }default:{
+        console.log('Unexpected id encountered in info menu dropdown = ' + id);
+        break;
+      }
+    }
+
+    this.graphService.updateSelectedCellInfo(this.glyphInfo);
+  }
+
+  inputChange(event: any) {
     const id = event.target.id;
 
     switch (id) {
+      case 'displayID':{
+        this.glyphInfo.displayID;
+        break;
+      }
       case 'name': {
         this.glyphInfo.name = event.target.value;
         break;
@@ -33,8 +92,12 @@ export class InfoEditorComponent implements OnInit {
         this.glyphInfo.description = event.target.value;
         break;
       }
+      case 'version':{
+        this.glyphInfo.version = event.target.value;
+        break;
+      }
       default: {
-        console.log('Unexpected id encountered in info menu = ' + id);
+        console.log('Unexpected id encountered in info menu input = ' + id);
         break;
       }
     }
