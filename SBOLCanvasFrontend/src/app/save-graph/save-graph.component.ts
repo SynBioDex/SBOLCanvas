a47d729af06eee078eb2fb04a8225a0eec9b63ad
Sam Thayer <samonella7@gmail.com>
2019-09-29 14:24:27 -0600

    Upgrade color menu to design menu stub

--- SBOLCanvasFrontend/src/app/save-graph/save-graph.component.ts
+++ SBOLCanvasFrontend/src/app/save-graph/save-graph.component.ts
@@ -2,7 +2,6 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import { SaveDialogData } from '../toolbar/toolbar.component';
 
-
 @Component({
   selector: 'app-save-graph',
   templateUrl: './save-graph.component.html',
