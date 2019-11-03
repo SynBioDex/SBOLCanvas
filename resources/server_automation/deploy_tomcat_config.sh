74665da9abe8ff1103661ce8f1c90a143c32e649
jaearl <jared.s.earl123@gmail.com>
2019-11-02 23:10:14 -0600

    Get SSL working on the server and develop automation around the tomcat configuration

--- resources/server_automation/deploy_tomcat_config.sh
+++ resources/server_automation/deploy_tomcat_config.sh
@@ -9,18 +9,21 @@ ensure_superuser
 
 echo "----------- Deploying tomcat... ------------"
 
-scp -P ${SERVER_SSH_PORT} ${TOMCAT_AUTOMATION_DIR}/base_config_files/tomcat-users.xml ${TOMCAT_AUTOMATION_DIR}/base_config_files/server.xml root@${SERVER_ADDRESS}:${TOMCAT_SERVER_DIR}/conf/ \
+scp -P ${SERVER_SSH_PORT} ${TOMCAT_AUTOMATION_DIR}/base_config_files/tomcat-users.xml ${TOMCAT_AUTOMATION_DIR}/base_config_files/server.xml ${TOMCAT_AUTOMATION_DIR}/base_config_files/web.xml root@${SERVER_ADDRESS}:${TOMCAT_SERVER_DIR}/conf/ \
 || die "Failed to scp tomcat base config files to server"
 scp -P ${SERVER_SSH_PORT} ${TOMCAT_AUTOMATION_DIR}/base_config_files/manager-context.xml root@${SERVER_ADDRESS}:${TOMCAT_SERVER_DIR}/webapps/manager/META-INF/context.xml \
 || die "Failed to scp tomcat manager config to server"
 scp -P ${SERVER_SSH_PORT} ${TOMCAT_AUTOMATION_DIR}/base_config_files/host-manager-context.xml root@${SERVER_ADDRESS}:${TOMCAT_SERVER_DIR}/webapps/host-manager/META-INF/context.xml \
 || die "Failed to scp tomcat host-manager config to server"
 
-scp -P ${SERVER_SSH_PORT} ${TOMCAT_AUTOMATION_DIR}/base_config_files/tomcat.service /etc/systemd/system/ \
+scp -P ${SERVER_SSH_PORT} ${TOMCAT_AUTOMATION_DIR}/base_config_files/tomcat.service root@${SERVER_ADDRESS}:/etc/systemd/system/ \
 || die "Failed to scp systemd unit file for tomcat to server"
 
 # Runs script on the server directly.
 ssh -p ${SERVER_SSH_PORT} root@${SERVER_ADDRESS} /bin/bash -s < ./runs_on_target_machine/refresh_tomcat.sh root \
 || die "Failed to refresh systemd tomcat service on server"
 
-echo "-------- Tomcat successfully deployed ---------"
+ssh -p ${SERVER_SSH_PORT} root@${SERVER_ADDRESS} /bin/bash -s < ./runs_on_target_machine/tomcat_configure_firewall.sh root \
+|| die "Failed to configure firewall on server"
+
+echo "-------- Tomcat configuration successfully deployed ---------"
--- resources/server_automation/runs_on_target_machine/tomcat_configure_firewall.sh
+++ resources/server_automation/runs_on_target_machine/tomcat_configure_firewall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# This is how the firewall is managed on CentOS 7. We can run this
+# as many times as we want with no harm done.
+firewall-cmd --zone=public --permanent --add-port=80/tcp
+firewall-cmd --zone=public --permanent --add-port=443/tcp
+firewall-cmd --zone=public --permanent --add-port=8443/tcp
 firewall-cmd --zone=public --permanent --add-port=8080/tcp
 firewall-cmd --reload
