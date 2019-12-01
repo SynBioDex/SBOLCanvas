4619eeeec98bc82db947454339eee19ce76093f9
Sam Thayer <samonella7@gmail.com>
2019-11-30 17:23:07 -0700

    Fixes pending-changes warning, and removes /home route

--- SBOLCanvasFrontend/src/app/pending-changes.guard.ts
+++ SBOLCanvasFrontend/src/app/pending-changes.guard.ts
@@ -9,12 +9,16 @@ export interface ComponentCanDeactivate {
 @Injectable()
 export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
   canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
+
     // if there are no pending changes, just allow deactivation; else confirm first
-    return component.canDeactivate() ?
-      true :
+      console.log("NOpe\n");
+    if (component.canDeactivate()) {
+      return true;
+    } else {
       // NOTE: this warning message will only be shown when navigating elsewhere within your angular app;
       // when navigating away from your angular app, the browser will show a generic warning message
       // see http://stackoverflow.com/a/42207299/7307355
-      confirm('WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.');
+      return confirm('WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.');
+    }
   }
 }
