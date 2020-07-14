bcd6b4dde2c01d4f30a15c8d179fe9d643a51dc0
Logan Terry <randoom97@live.com>
2020-07-14 16:05:07 -0600

    Tutorial and About open in new tabs, and added SBOL Industrial Consotrium acknowledgement

--- SBOLCanvasFrontend/src/app/landing-page/landing-page.component.ts
+++ SBOLCanvasFrontend/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) { 
+    this.titleService.setTitle("SBOL Canvas About");
+  }
 
   ngOnInit() {
   }
