b6a7cbb382c7af2ba3a6e2ec83036169c9675394
Zach Sents <zachsents@gmail.com>
2022-03-28 21:20:17 -0600

    Added URL param option ignoreHTTPErrors

--- SBOLCanvasFrontend/src/app/http.interceptor.ts
+++ SBOLCanvasFrontend/src/app/http.interceptor.ts
@@ -4,10 +4,19 @@ import { Observable, of, throwError } from "rxjs";
 import { tap, catchError } from "rxjs/operators";
 import { MatDialog } from '@angular/material';
 import { ErrorComponent } from './error/error.component';
+import { ActivatedRoute } from '@angular/router'
+
 
 @Injectable()
 export class AppHttpInterceptor implements HttpInterceptor {
-    constructor(public dialog: MatDialog) {}
+
+    ignoreHTTPErrors: boolean
+
+    constructor(public dialog: MatDialog, private route: ActivatedRoute) {
+        this.route.queryParams.subscribe(params => {
+            this.ignoreHTTPErrors = !!params.ignoreHTTPErrors
+        })
+    }
 intercept(
         req: HttpRequest<any>,
         next: HttpHandler
@@ -19,7 +28,7 @@ intercept(
             }),
             catchError((err: any) => {
                 if(err instanceof HttpErrorResponse && err.status === 500) {
-                    this.dialog.open(ErrorComponent, {data: err.error});
+                    !this.ignoreHTTPErrors && this.dialog.open(ErrorComponent, {data: err.error});
                 }
                 return throwError(err);
             }));
