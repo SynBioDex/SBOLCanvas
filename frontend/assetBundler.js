5bc72450bb8b49dbdeca6060cd1ee2f6df5a3495
Zach Sents <zachsents@gmail.com>
2022-08-22 16:46:43 -0600

    Added XML bundling during prebuild

---
+++ frontend/assetBundler.js
@@ -0,0 +1,91 @@
+const fs = require("fs").promises
+const path = require("path")
+
+const BUNDLE_DIRS = [
+    "src/assets/glyph_stencils/sequence_feature",
+    "src/assets/glyph_stencils/molecular_species",
+    "src/assets/glyph_stencils/interactions",
+    "src/assets/glyph_stencils/interaction_nodes",
+    "src/assets/glyph_stencils/indicators",
+    "src/assets",
+]
+
+const BUNDLE_FILENAME = "bundle.xml"
+const BUNDLE_ORDER_FILENAME = ".bundle_order"
+
+{
+    (async () => {
+        for (const dir of BUNDLE_DIRS)
+            await bundleDir(dir)
+    })()
+}
+
+async function bundleDir(dir) {
+    console.log("\nBundling assets in", dir)
+
+    // read in all files in directory
+    const files = await fs.readdir(dir)
+    console.log("\tFound", files.length, "files.")
+
+    // try to sort files according to .bundle_order file
+    try {
+        // read in file
+        const bundleOrderContent = (await fs.readFile(path.join(dir, BUNDLE_ORDER_FILENAME), "utf8"))
+            .split(/\r?\n/)                // split by line
+            .filter(item => !!item.trim()) // remove blanks
+        const bundleOrder = bundleOrderContent.filter(item => !item.startsWith("!"))
+        console.log("\tUsing bundle order.")
+
+        // find exclusions
+        const exclude = bundleOrderContent
+            .filter(item => item.startsWith("!"))
+            .map(item => item.replace("!", ""))
+
+        // sort files array
+        const findOrderIndex = fileName => {
+            const index = bundleOrder.findIndex(item => fileName.includes(item))
+            return index == -1 ? bundleOrder.length + 1 : index
+        }
+        files.sort(
+            (a, b) => findOrderIndex(a) - findOrderIndex(b)
+        )
+
+        // filter out files that are in the exclusions
+        const filesExcluded = []
+        exclude.forEach(exclusion => {
+            const index = files.findIndex(file => file.includes(exclusion))
+            index != -1 && filesExcluded.push(files.splice(index, 1))
+        })
+        console.log("\tExcluding", filesExcluded.length, "files.")
+    }
+    catch (err) { console.error(err) }
+
+    // await all file content to be loaded in
+    const filesContents = await Promise.all(
+        files
+            // grab only XMLs
+            .filter(fileName => fileName.endsWith('.xml'))
+            // make sure we don't grab a preexisting bundle
+            .filter(fileName => fileName != BUNDLE_FILENAME)
+            // read in file content
+            .map(fileName => fs.readFile(path.join(dir, fileName), "utf8"))
+    )
+
+    // combine content
+    const joinedContent = filesContents
+        // remove <shapes> tag
+        .map(content => content.replace(/\<[\/]*shapes\>/g, ""))
+        // combine into one string
+        .join("\n")
+
+    // write bundle out
+    const bundlePath = path.join(dir, BUNDLE_FILENAME)
+    await fs.writeFile(
+        bundlePath,
+        `<shapes>
+        ${joinedContent}
+</shapes>`
+    )
+
+    console.log("\tWrote bundle:", bundlePath)
+}
