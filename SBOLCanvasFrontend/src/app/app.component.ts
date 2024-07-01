aabf985b9990a69097346a7d995be2fc2ea68814
Derick Sayavong <dsayavong54651@gmail.com>
2024-07-01 14:39:00 -0600

    we are cooking

--- SBOLCanvasFrontend/src/app/app.component.ts
+++ SBOLCanvasFrontend/src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
+declare global {
+  interface Window {
+    mxBasePath: string;
+    mxImageBasePath: string;
+    mxLoadResources: boolean;
+    mxForceIncludes: boolean;
+    mxLoadStylesheets: boolean;
+    mxResourceExtension: string;
+  }
+}
 
+window.mxBasePath = 'mxgraph';
+window.mxImageBasePath = 'mxgraph/images'
+window.mxLoadResources = true;
+window.mxForceIncludes = false;
+window.mxLoadStylesheets = true;
+window.mxResourceExtension = '.txt';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
