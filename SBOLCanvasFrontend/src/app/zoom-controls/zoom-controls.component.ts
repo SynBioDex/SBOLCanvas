96cff69b8bdb7d5dde450adae524b10e1be46b54
Derick Sayavong <dsayavong54651@gmail.com>
2024-07-03 11:52:48 -0600

    Zoom controls updated

--- SBOLCanvasFrontend/src/app/zoom-controls/zoom-controls.component.ts
+++ SBOLCanvasFrontend/src/app/zoom-controls/zoom-controls.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild  } from '@angular/core'
 import { GraphService } from '../graph.service'
+import { MatTooltip } from '@angular/material/tooltip';
 
 
 @Component({
@@ -9,32 +10,39 @@ import { GraphService } from '../graph.service'
 })
 
 export class ZoomControlsComponent {
-
+    @ViewChild("tooltip") tooltip: MatTooltip
     constructor(public graphService: GraphService) { }
 
     zoomSliderChanged($event) {
         this.graphService.setZoom($event.value / 100);
     }
-
+    
     zoomInputChanged($event) {
         let number = parseInt($event.target.value);
         if (!isNaN(number)) {
             const percent = number / 100;
             this.graphService.setZoom(percent);
         }
-
         // if they entered nonsense the zoom doesn't change, which
         // means angular won't refresh the input box on its own
         $event.target.value = this.getZoomDisplayValue();
     }
-
+    
     getZoomSliderValue() {
         return this.graphService.getZoom() * 100;
     }
 
     getZoomDisplayValue() {
-        let percent = this.graphService.getZoom() * 100;
+        const maxZoom = 4
+        let percent = Math.min(this.graphService.getZoom(), maxZoom) * 100;
         let string = percent.toFixed(0);
         return string.toString() + '%';
+    }
+
+    displayTooltip(){
+        this.tooltip.show()
+        setTimeout(() => {
+          this.tooltip.hide()
+        }, 2000);
     }
 }
