8fa7058a29c049b73376e2cc1b9a1b36b370158d
Derick Sayavong <dsayavong54651@gmail.com>
2024-09-23 21:09:02 -0600

    Reverse Complement sequences fixed

--- SBOLCanvasFrontend/src/app/info-editor/info-editor.component.ts
+++ SBOLCanvasFrontend/src/app/info-editor/info-editor.component.ts
@@ -60,7 +60,7 @@ export class InfoEditorComponent implements OnInit {
   }
 
   getTypes() {
-    this.metadataService.loadTypes().subscribe(types => this.partTypes = types);
+    this.metadataService.loadTypes().subscribe(types => this.partTypes = types.filter(type => type != "Circular"));
   }
 
   getRoles() {
