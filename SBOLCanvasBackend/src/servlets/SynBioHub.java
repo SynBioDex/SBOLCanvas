569ab068421f86bd774a76ab31633375f1cee537
Kenzo-Schwab <kenzoschwab@gmail.com>
2025-09-26 09:43:23 -0600

    this could work

--- SBOLCanvasBackend/src/servlets/SynBioHub.java
+++ SBOLCanvasBackend/src/servlets/SynBioHub.java
@@ -95,8 +95,11 @@ public class SynBioHub extends HttpServlet {
 					try {
 						sbhf.login(email, password);
 					} catch (SynBioHubException e) {
-						if (e.getMessage().equals("org.synbiohub.frontend.PermissionException")) {
+						if (e.getMessage().equals("An error has occured. Please log back into SynbBioHub")) {//used to be "org.synbiohub.frontend.PermissionException"
 							response.setStatus(HttpStatus.SC_UNAUTHORIZED);
+							email = null;
+							password = null;
+							user = null;
 							return;
 						}
 						throw e;
@@ -291,8 +294,11 @@ public class SynBioHub extends HttpServlet {
 			ServletOutputStream outputStream = response.getOutputStream();
 			InputStream inputStream = new ByteArrayInputStream(e.getMessage().getBytes());
 			IOUtils.copy(inputStream, outputStream);
-			if(e.getMessage().equals("org.synbiohub.frontend.PermissionException")){
+			if(e.getMessage().equals("An error has occured. Please log back into SynbBioHub")) {//used to be "org.synbiohub.frontend.PermissionException"
 				response.setStatus(HttpStatus.SC_UNAUTHORIZED);
+				email = null;
+				password = null;
+				user = null;
 			}
 			else{
 				response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
