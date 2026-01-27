d9fd81bdb4b48bf72fe53bfb73e5507f2bf62306
Travis Uhrig <travisformayor@users.noreply.github.com>
2026-01-27 14:25:10 -0700

    handle missing description in errors

--- SBOLCanvasBackend/src/servlets/Convert.java
+++ SBOLCanvasBackend/src/servlets/Convert.java
@@ -79,7 +79,8 @@ public class Convert extends HttpServlet {
 		} catch (SBOLValidationException | IOException | SBOLConversionException | ParserConfigurationException
 				| TransformerException | SAXException | TransformerFactoryConfigurationError | URISyntaxException | SynBioHubException | javax.xml.stream.XMLStreamException e) {
 			ServletOutputStream outputStream = response.getOutputStream();
-			InputStream inputStream = new ByteArrayInputStream(e.getMessage().getBytes());
+			String message = e.getMessage() != null ? e.getMessage() : "Export failed";
+			InputStream inputStream = new ByteArrayInputStream(message.getBytes());
 			IOUtils.copy(inputStream, outputStream);
 
 			response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
